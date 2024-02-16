5a1f7822ca55d54109f0e47aa874f6ca79e3c14c
saida <sa1018@exeter.ac.uk>
2024-02-16 17:45:54 +0000

    .

--- beegarden/beegarden/settings.py
+++ beegarden/beegarden/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'landingpage'
+    'landingpage',
     'accounts',
     'widget_tweaks'
 ]
