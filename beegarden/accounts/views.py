38389bac661bd4d2bca60b465249f84c17d4815b
deeniaffendi <ndb205@exeter.ac.uk>
2024-02-17 22:38:04 +0000

    css

--- beegarden/accounts/views.py
+++ beegarden/accounts/views.py
@@ -5,7 +5,6 @@ from django.views.generic import CreateView
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView as AuthLoginView
 
-
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
--- beegarden/beegarden/settings.py
+++ beegarden/beegarden/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'landingpage'
+    'landingpage',
     'accounts',
-    'widget_tweaks'
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 # Default primary key field type
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "home" 
-LOGOUT_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "landing"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
