4c484d0c99c0ddd50e2b7b73a2742c9fc10347a7
Walis1010 <wariszaluck9@gmail.com>
2024-02-19 13:03:47 +0000

    update model for very simple user account

--- beegarden/accounts/models.py
+++ beegarden/accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    # Will extend player user and admin user in the future
+    #fields needed for the user model
+    score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.username
+
+class Garden(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username + "'s garden"
+
+class Bee(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    health = models.IntegerField(default=100)
+    pollen = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username + "'s bee"
