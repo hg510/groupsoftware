c8670c37172e2d28c13eb6c2e7f52665c0122c04
Walis1010 <wariszaluck9@gmail.com>
2024-02-24 20:38:05 +0000

    (not complete) version of leaderboard

--- beegarden/leaderboard/views.py
+++ beegarden/leaderboard/views.py
@@ -1,3 +1,10 @@
 from django.urls import path
 from . import views
 
+from django.shortcuts import render
+from .models import Leaderboard
+
+def leaderboard(request):
+    # Get the leaderboard that display the user's score from top to bottom (Ex. Rank one is the player with highest score)
+    leaderboard = Leaderboard.objects.order_by('-score')
+    return render(request, 'leaderboard.html', {'leaderboard': leaderboard})
