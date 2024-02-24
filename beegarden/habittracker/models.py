2236ac2b92c42f81bad7e32676b5e5bd69c7dae6
deeniaffendi <ndb205@exeter.ac.uk>
2024-02-24 21:17:46 +0000

    add score function to the habit tracker

--- beegarden/habittracker/models.py
+++ beegarden/habittracker/models.py
@@ -1,12 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Habit(models.Model):
     walk = models.BooleanField(default=False)
     plastic = models.BooleanField(default=False)
     food = models.BooleanField(default=False)
+    water_bottle = models.BooleanField(default=False)
+    utensils = models.BooleanField(default=False)
+    short_trips = models.BooleanField(default=False)
+    plant_based_meals = models.BooleanField(default=False)
+    unplug_electronics = models.BooleanField(default=False)
+    natural_light = models.BooleanField(default=False)
+    community_cleanups = models.BooleanField(default=False)
+    reduce_food_waste = models.BooleanField(default=False)
+    public_transportation = models.BooleanField(default=False)
+    turn_off_lights = models.BooleanField(default=False)
     
     # Add a field for date created
     date_created = models.DateTimeField(auto_now_add=True)
 
     # Make the user field mandatory
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class UserScore(models.Model):
+    score = models.IntegerField(default=0)
+    streak_count = models.IntegerField(default=0)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"User: {self.user.username}, Score: {self.score}, Streak Count: {self.streak_count}"
