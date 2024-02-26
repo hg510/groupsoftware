9ea8dae98d02f9845cd4f2c08cbdd872eabb6e1c
Walis1010 <wariszaluck9@gmail.com>
2024-02-26 10:48:17 +0000

    Updates in Garden's models, utils, signals, apps

--- beegarden/garden/models.py
+++ beegarden/garden/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-
 class Seed(models.Model):
     RARITY_CHOICES = [
         ('common', 'Common'),
@@ -16,6 +15,26 @@ class Seed(models.Model):
     def __str__(self):
         return f"{self.name} ({self.rarity})"
 
+# Now, the idea is that we have a single seed icon and when the player drags it will give the seed in order of receiving
+# Like if player has 6 seeds so far and already plant the first 3, this time they will drag and drop and get the 4th seed
+# This is because I don't know if we have enough time for the front end inventory system to allow users with multiple seeds option out of what they own
+# class SeedInventory(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     seed = models.ForeignKey(Seed, on_delete=models.CASCADE)
+#     quantity = models.IntegerField(default=0)
+
+#     def __str__(self):
+#         return f"{self.user.username} - {self.seed.name} x{self.quantity}"
+
+class SeedInventory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='seed_inventory')
+    seed = models.ForeignKey(Seed, on_delete=models.CASCADE, related_name='inventory_items')
+    # Each time a seed that the user have never own (or currently doesn't own a single one) is award, we add the new one
+    # starting with 1, then increment if the user get more of that seed in the future
+    quantity = models.IntegerField(default=1)  
+
+    def __str__(self):
+        return f"{self.user.username} - {self.seed.name} x{self.quantity}"
 
 class UserGarden(models.Model):
     PLANT_STATUS_CHOICES = [
