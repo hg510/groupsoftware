fa5325142f3b0b18ee3d5f09e6dde57432a3a450
hg510 <114531196+hg510@users.noreply.github.com>
2024-02-27 18:46:16 +0000

    Update views.py

--- beegarden/adminpage/views.py
+++ beegarden/adminpage/views.py
@@ -1,27 +1,31 @@
 from django.utils import timezone
 from django.shortcuts import render, redirect
-from .models import Request, Response
+from .models import Request, Response, 
 
 def adminPage(request):
     user = request.user
-    today = timezone.now().date()
-    form_submitted = Response.objects.filter(user=user, date_created__date=today).exists()
 
-    if request.method == 'POST':
-        if form_submitted:
-            return render(request, 'adminPage.html', {'form_submitted': True})
+    #Check if user is admin
+    admin = Request.objects.get(id=user)
+    if admin.is_admin:
+        today = timezone.now().date()
+        form_submitted = Response.objects.filter(user=user, date_created__date=today).exists()
 
-        id = request.POST.get('id')
-        accepted = request.POST.get('accepted') == 'on'
+        if request.method == 'POST':
+            if form_submitted:
+                return render(request, 'adminPage.html', {'form_submitted': True})
 
-        # Create Response object
-        response = Response.objects.create(id=id, accepted=accepted)
+            id = request.POST.get('id')
+            accepted = request.POST.get('accepted') == 'on'
 
-        # Delete request
-        delete_request(id)
+            # Create Response object
+            response = Response.objects.create(id=id, accepted=accepted)
 
-    return render(request, 'adminPage.html', {'form_submitted': form_submitted})
+            # Delete request
+            delete_request(id)
 
+        return render(request, 'adminPage.html', {'form_submitted': form_submitted})
+    return render(request, 'home.html')
 
 def delete_request(id):
     goalRequest = Request.objects.get(id=id)
