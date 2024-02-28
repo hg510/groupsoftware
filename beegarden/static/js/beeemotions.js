c037f2f4b0c2db5df08c139d060383c10aa3f204
deeniaffendi <ndb205@exeter.ac.uk>
2024-02-28 17:27:33 +0000

    added buttons to page and edit bee emotions

--- beegarden/static/js/beeemotions.js
+++ beegarden/static/js/beeemotions.js
@@ -40,6 +40,20 @@ function pet(path) {
     }
   }  
 
+  // Mock happiness score
+// let happinessScore = 70; // Assume initial happiness score
+
+// Update the health bar based on happiness score
+function updateHealthBar(userScore) {
+  const healthBar = document.getElementById('health');
+  // Adjust the health bar width based on the user score
+  healthBar.style.width = userScore + '%';
+}
+
+// Initial update based on the happiness score (assuming this is the user's score)
+updateHealthBar(happinessScore);
+
+
 // //chabges background based on season
 // var currentdate = new Date();
 // var season = currentdate.getMonth()
