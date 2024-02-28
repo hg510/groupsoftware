18455fdb080d2296297af062d2fafb342927e2ea
Walis1010 <wariszaluck9@gmail.com>
2024-02-28 12:30:16 +0000

    take out server log debugger to avoid extensive terminal message

--- beegarden/beegarden/settings.py
+++ beegarden/beegarden/settings.py
@@ -26,24 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Add or modify the logging settings 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
 
 
 # Application definition
