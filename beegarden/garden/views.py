eb4686b622b172fc10df3cc5ae64f0ca3bcd3a88
Walis1010 <wariszaluck9@gmail.com>
2024-03-12 19:46:26 +0000

    fix adminpage invisibility & fix virtual garden page not loaded when user has 0 score

--- beegarden/garden/views.py
+++ beegarden/garden/views.py
@@ -1,12 +1,26 @@
 # Author: Nur Deeni
 
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from habittracker.models import UserScore
 
+# Code version where the virtual garden will still load although the user has 0 score
 def garden_view(request):
-    # Fetch the user's score
-    user_score = UserScore.objects.get(user=request.user)
-    score = user_score.score
-    
+    try:
+        # Try to fetch the user's score
+        user_score = UserScore.objects.get(user=request.user)
+        score = user_score.score
+    except ObjectDoesNotExist:
+        # If the UserScore object does not exist, set the score to 0
+        score = 0
+
     # Pass the score to the template
     return render(request, 'garden/garden.html', {'score': score})
+
+# def garden_view(request):
+#     # Fetch the user's score
+#     user_score = UserScore.objects.get(user=request.user)
+#     score = user_score.score
+    
+#     # Pass the score to the template
+#     return render(request, 'garden/garden.html', {'score': score})
