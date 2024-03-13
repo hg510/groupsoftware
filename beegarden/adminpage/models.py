3028984f713fddb66703f02638bb6b4513740b7e
Walis1010 <wariszaluck9@gmail.com>
2024-03-13 07:44:05 +0000

    update hte admin page logic & fix model naming conflict bugs

--- beegarden/adminpage/models.py
+++ beegarden/adminpage/models.py
@@ -4,26 +4,24 @@ from django.db import models
 from django.contrib.auth.models import User
 from habittracker.models import Habit  
 
-
 # Represents a request made for reviewing a habit submission.
-class Request(models.Model):
+class HabitRequest(models.Model):
     goal = models.CharField(max_length=150, verbose_name="Goal")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='requests', verbose_name="User")
-    habit = models.ForeignKey(Habit, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Habit")  # Add this line
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='habit_requests', verbose_name="User")
+    habit = models.ForeignKey(Habit, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Habit")
 
     class Meta:
-        verbose_name = "Request"
-        verbose_name_plural = "Requests"
+        verbose_name = "Habit Request"
+        verbose_name_plural = "Habit Requests"
 
-
-# Represents an admin's response to a request, indicating acceptance or rejection.
-class Response(models.Model):
+# Represents an admin's response to a habit request, indicating acceptance or rejection.
+class ResponseRequest(models.Model):
     accepted = models.BooleanField(verbose_name="Accepted")
     date_created = models.DateTimeField(auto_now_add=True, verbose_name="Date Created")
-    request = models.ForeignKey(Request, on_delete=models.CASCADE, related_name='responses', verbose_name="Request")
+    habit_request = models.ForeignKey(HabitRequest, on_delete=models.CASCADE, related_name='response_requests', verbose_name="Habit Request")
 
     class Meta:
-        verbose_name = "Response"
-        verbose_name_plural = "Responses"
+        verbose_name = "Response Request"
+        verbose_name_plural = "Response Requests"
 
 
--- beegarden/beegarden/settings.py
+++ beegarden/beegarden/settings.py
@@ -47,13 +47,21 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    # Change the order of processor to avoid error
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.security.SecurityMiddleware',
+    # 'django.contrib.sessions.middleware.SessionMiddleware',
+    # 'django.middleware.common.CommonMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # 'django.contrib.messages.middleware.MessageMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'beegarden.urls'
