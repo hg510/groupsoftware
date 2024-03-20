f028f44423fedf966dfe60ebbd941b81e61c6345
Walis1010 <wariszaluck9@gmail.com>
2024-03-20 19:08:13 +0000

    ensure seed model allignment with map branch

--- beegarden/garden/urls.py
+++ beegarden/garden/urls.py
@@ -2,12 +2,12 @@
 
 from django.urls import path
 from . import views
-from .views import check_for_seed_award
 
 urlpatterns = [
     path('', views.garden_view, name='garden'),
-    # path('has_approved_requests/', views.has_approved_requests, name='has_approved_requests'),
-    path('check_for_seed_award/', check_for_seed_award, name='check_for_seed_award'),
     path('save_planted_seed/', views.save_planted_seed, name='save_planted_seed'),
     path('load_planted_seeds/', views.get_planted_seeds, name='planted_seeds'),    
+    path('userSeeds/', views.userSeeds, name='userSeeds'),  
+    path('clearUserSeeds/', views.clearUserSeeds, name='clearUserSeeds'),    
+    path('updateDisplayedSeeds/', views.updateDisplayedSeeds, name='updateDisplayedSeeds'),   
 ]
